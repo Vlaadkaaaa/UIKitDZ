45888043e87b0fa63f803005209553e28c34f771
Vladka <vlad.lymar@list.ru>
2022-10-04 12:28:37 +0500

    Небольшие правки

--- UIKitDZ/UIKitDZ/VC/SignInViewController.swift
+++ UIKitDZ/UIKitDZ/VC/SignInViewController.swift
@@ -14,6 +14,7 @@ final class SignInViewController: UIViewController {
     var users = Users()
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordTextField.isSecureTextEntry = true
         
     }
     
