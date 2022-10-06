88694bed02d1de6bb680cd04f5ede33921c4d194
Vladka <vlad.lymar@list.ru>
2022-10-06 17:57:42 +0500

    Таймер

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -9,14 +9,15 @@ import UIKit
 
 ///
 final class ViewController: UIViewController {
+  
     // MARK: - IBOutlet
-    
-    // MARK: - Visual Components
-    
-    // MARK: - Public Properties
+    @IBOutlet weak var timerLabel: UILabel!
+
+    @IBOutlet weak var startOutlet: UIButton!
     
     // MARK: - Private Properties
-    
+    private var timer: Timer?
+    private var currentTime: Float = 0.0
     // MARK: - Initializers
     
     // MARK: - UIViewController (*)
@@ -29,11 +30,25 @@ final class ViewController: UIViewController {
     
     // MARK: - IBAction
     
+    @IBAction func resetAction(_ sender: Any) {
+    }
+    @IBAction func startAction(_ sender: Any) {
+        startTimer()
+    }
     // MARK: - Private Methods
-    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                     target: self, selector: #selector(updateTimer),
+                                    userInfo: nil, repeats: true)
+    }
+    @objc private func updateTimer(sender: Timer) {
+        currentTime += Float(sender.timeInterval)
+    }
+    private func updateLabel() {
+        timerLabel.text = "\(currentTime)"
+    }
     // MARK: - Types
     
     // MARK: - Constants
-    
     
 }
